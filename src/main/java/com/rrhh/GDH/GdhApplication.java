4c6ab82cbc06c4f16ba26c2ea4084a0999d3b8ac
Harold RM <haroldferrm@gmail.com>
2023-09-17 21:51:03 -0500

    x

---
+++ src/main/java/com/rrhh/GDH/GdhApplication.java
@@ -0,0 +1,13 @@
+package com.rrhh.GDH;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GdhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GdhApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rrhh/GDH/GdhApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rrhh.GDH;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GdhApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
